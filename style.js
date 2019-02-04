3fa9473468a53115199db12ef1d18d2f27a4cc8f
maiderguerrero <47318536+maiderguerrero@users.noreply.github.com>
2019-02-04 13:16:47 +0100

    Update style.js

--- style.js
+++ style.js
@@ -24,6 +24,8 @@ styles: [
 				{ name: 'Lista Ordenada 2', element: 'ol', attributes: { 'class': 'bck-ol bck-ol-2' } },
 				{ name: 'Lista Ordenada 3', element: 'ol', attributes: { 'class': 'bck-ol bck-ol-3' } },
 				{ name: 'Lista Ordenada 4', element: 'ol', attributes: { 'class': 'bck-ol bck-ol-4' } },
+				{ name: 'Lista Ordenada 5', element: 'ol', attributes: { 'class': 'bck-ol bck-ol-5' } },
+
 
 
 				{ name: 'Caja 1', type: 'widget', widget: 'blink_box', attributes: { 'class': 'bck-box bck-box-1' } },
